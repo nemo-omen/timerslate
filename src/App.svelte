71433cccec8cd5b996226ffbf1f23c3dfc9ad9ee
nemo-omen <nemo.omen@gmail.com>
2023-08-08 13:05:18 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import CountdownTimer from './lib/CountdownTimer.svelte';
+  const live = new Date('August 10, 2023 18:30:00');
+</script>
+
+<main>
+  <img
+    class="slate"
+    src="https://www.conchovalleyhomepage.com/wp-content/uploads/sites/83/2023/08/HSFBPS_Logo.png"
+    alt="High School Football Preview Show"
+  />
+  <CountdownTimer expires={live} />
+  <img
+    class="logo"
+    src="https://www.conchovalleyhomepage.com/wp-content/uploads/sites/83/2023/08/TwistedRootBurger_Logo_white.png"
+    alt="Twisted Root Burger Company"
+  />
+</main>
+
+<style>
+  main {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    /* margin: 3rem 0 4rem 0; */
+  }
+
+  .slate {
+    max-width: 46rem;
+    margin-top: 3rem;
+  }
+
+  .logo {
+    max-width: 11rem;
+    margin-bottom: 8rem;
+  }
+</style>
---
+++ src/lib/CountdownTimer.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { fade } from 'svelte/transition';
+  export let expires: Date;
+
+  type TimeObject = {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  };
+
+  const now = new Date();
+  const currentTime = now.getTime();
+  const liveTime = expires.getTime();
+  let timeRemaining = liveTime - currentTime;
+  const slateCutoff = 15 * 1000 * 60;
+  console.log({ timeRemaining });
+
+  $: currentTimeObject = { ...getTime() };
+  $: d = currentTimeObject.days;
+  $: h = currentTimeObject.hours;
+  $: m = currentTimeObject.minutes;
+  $: s = currentTimeObject.seconds;
+  $: showMessage = false;
+
+  function format(t: number): string {
+    return t < 10 ? '0' + t.toString() : t.toString();
+  }
+
+  function getTime(): TimeObject {
+    return {
+      days: Math.floor(timeRemaining / 1000 / 60 / 60 / 24),
+      hours: Math.floor(timeRemaining / 1000 / 60 / 60) % 24,
+      minutes: Math.floor(timeRemaining / 1000 / 60) % 60,
+      seconds: Math.floor(timeRemaining / 1000) % 60,
+    };
+  }
+
+  function update() {
+    currentTimeObject = { ...getTime() };
+  }
+
+  function complete() {
+    showMessage = true;
+  }
+
+  function start() {
+    update();
+
+    const intervalId = setInterval(() => {
+      timeRemaining -= 1000;
+      if (timeRemaining < 0) {
+        complete();
+        clearInterval(intervalId);
+      } else {
+        update();
+      }
+    }, 1000);
+  }
+
+  onMount(() => {
+    start();
+  });
+</script>
+
+{#if timeRemaining > slateCutoff}
+  <img
+    transition:fade={{ duration: 500 }}
+    class="overlay-img"
+    src="https://www.conchovalleyhomepage.com/wp-content/uploads/sites/83/2023/08/hsfb-preview-show-fs.png"
+    alt="HSFBPS"
+  />
+{/if}
+
+<div class="timer">
+  {#if !showMessage}
+    <div class="time">
+      <!-- <span class="day">
+          {expires.toLocaleDateString('en-US', { month: 'long', day: 'numeric', weekday: 'long' })}
+          {expires.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' })}
+        </span> -->
+      {#if h > 0}
+        <span>
+          {format(h)}:
+        </span>
+      {/if}
+      <span>
+        {format(m)}:
+      </span>
+      <span>
+        {format(s)}
+      </span>
+    </div>
+  {:else}
+    <span in:fade={{ duration: 200 }}>The show starts now!</span>
+  {/if}
+</div>
+
+<style>
+  .timer {
+    font-family: monospace;
+    align-self: stretch;
+    font-size: 7rem;
+    font-weight: 800;
+    color: #ffffff;
+    text-transform: uppercase;
+    letter-spacing: 0.125em;
+  }
+
+  .time {
+    display: flex;
+    justify-content: center;
+  }
+
+  span {
+    display: block;
+    text-align: center;
+  }
+
+  .overlay-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 1920px;
+    height: 1080px;
+    /* opacity: 0.1; */
+  }
+</style>
